53a9b12997bd732833c53fd9e84f96eeb620ce29
Kaushik <davidkgp@gmail.com>
2021-04-05 21:05:07 +0200

    initial s3 bucket

--- lib/cdk-doc-server-stack.ts
+++ lib/cdk-doc-server-stack.ts
@@ -1,9 +1,20 @@
+import { Bucket, BucketEncryption } from '@aws-cdk/aws-s3';
 import * as cdk from '@aws-cdk/core';
 
 export class CdkDocServerStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const docStorageBucket = new Bucket(this, "DocBucket", 
+    {
+      encryption: BucketEncryption.S3_MANAGED
+    });
+
+    new cdk.CfnOutput(this,"DocBucketNameExport", {
+      value: docStorageBucket.bucketName,
+      exportName: "DocBucketName"
+
+    })
+
   }
 }
